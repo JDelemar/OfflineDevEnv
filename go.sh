3a4077be00cf19c162c12ba929ec80b31c21f6ea
Del <delemar@hotmail.com>
2017-06-11 09:21:46 -0400

    Update command

--- go.sh
+++ go.sh
@@ -2,7 +2,7 @@
 # go.sh
 # To test on play with docker (http://labs.play-with-docker.com), cut and paste this into the console starting with the next line uncommented
 # git clone https://github.com/JDelemar/OfflineDevEnv.git
-cd OfflineDevEnv/Offline 
+cd Offline 
 docker-compose up -d
 echo To check the status of local-npm: docker logs devnpm
 echo To check the status of the node container: docker logs devnode
